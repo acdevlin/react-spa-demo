c198813998a07e00e66edaec03213a3aced1c6b5
Alex Devlin <acdevlin42@gmail.com>
2026-08-12 11:19:08 -0700

    Cleans up styling
    Several classes were somehow missing from CSS files and causing unexpected behavior. This has been rectified.
    
    Also updated gitignore to include package-lock.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
+import '../stylesheets/Footer.css';
+
 const Footer = () => {
   return (
-    <footer className="theme-dark footer-0 text-center-desktop text-center-mobile">
-      <h2>ABC Limited</h2>
-      <h3>Awesome and affordable products</h3>
-      <div className="copyright">
+    <footer className="theme-dark text-center-desktop text-center-mobile">
+      <h2 className="footer-h2">ABC Limited</h2>
+      <h3 className="footer-h3">Awesome and affordable products</h3>
+      <div className="footer-copyright">
         <p>Copyright © 2026 ABC Limited. All rights reserved.</p>
       </div>
     </footer>
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,14 +1,17 @@
+import '../stylesheets/Petite.css';
+
+
 const PageNotFound = () => {
   return (
     <>
       <header className="theme-light text-center-desktop text-center-mobile">
         <div className="container-text">
-          <h1>Page Not Found 😭</h1>
+          <h1>404 - Page Not Found 😭</h1>
         </div>
       </header>
 
       <section class="w-820px">
-        <h3>Sorry, this page does not exist.</h3>
+        <p>Sorry, this page does not exist.</p>
       </section>
     </>
   );
